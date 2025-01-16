d21128fc639700b3aa7800da91ba3812836aded5
rafael <rcastanoblanca@safareyes.es>
2025-01-16 14:16:43 +0100

    pantalla principal en desarrollo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import {MenuComponent} from "./menu/menu.component";
+import {IonicModule} from "@ionic/angular";
+import {SuggestionsComponent} from "./suggestions/suggestions.component";
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    imports: [
+        MenuComponent,
+        IonicModule,
+        SuggestionsComponent
+    ],
+    standalone: true
 })
 export class AppComponent {
   constructor() {}
