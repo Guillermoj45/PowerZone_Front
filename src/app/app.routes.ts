08da66d8741d7d00837c8f64326758c6946e91fe
rafael <rcastanoblanca@safareyes.es>
2025-01-17 08:25:39 +0100

    pantalla principal ajustada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,18 +10,6 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
-  {
-    path: 'menu',
-    loadComponent: () => import('./menu/menu.component').then( m => m.MenuComponent)
-  },
-    {
-        path: 'suggestions',
-        loadComponent: () => import('./suggestions/suggestions.component').then( m => m.SuggestionsComponent)
-    },
-    {
-        path: 'menuoriginal',
-        loadComponent: () => import('./menuoriginal/menuoriginal.component').then( m => m.MenuoriginalComponent)
-    },
     {
         path: 'notification',
         loadComponent: () => import('./notification/notification.component').then( m => m.NotificationComponent)
