e43c931e5f601eb375bfea611f16342ad2dc3831
jmoca <114067390+jmoca@users.noreply.github.com>
2025-01-22 13:42:08 +0100

    página cada post

---
+++ src/app/Screen/post-details/post-details.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+    selector: 'app-post-details',
+    templateUrl: './post-details.component.html',
+    styleUrls: ['./post-details.component.scss'],
+    standalone: true,
+    imports: [CommonModule, IonicModule]
+})
+export class PostDetailsComponent implements OnInit {
+    post: any;
+
+    constructor(private router: Router) { }
+
+    ngOnInit(): void {
+        this.post = history.state.post;
+    }
+}
