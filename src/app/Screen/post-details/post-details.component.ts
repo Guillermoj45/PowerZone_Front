00fd9ebab89cc1b465f33d155211a5f53d1f39d7
guill <guillermoremesa@gmail.com>
2025-01-23 22:44:47 +0100

    creacion de componente post, problema, hay demasiados scroll

--- src/app/Screen/post-details/post-details.component.ts
+++ src/app/Screen/post-details/post-details.component.ts
@@ -5,34 +5,37 @@ import { IonicModule } from '@ionic/angular';
 import {Post} from "../../Models/Post";
 import {addIcons} from "ionicons";
 import {bookmark, chatbubble, heart, shareSocial} from "ionicons/icons";
+import {PublicationComponent} from "./publication/publication.component";
 
 @Component({
     selector: 'app-post-details',
     templateUrl: './post-details.component.html',
     styleUrls: ['./post-details.component.scss'],
     standalone: true,
-    imports: [CommonModule, IonicModule]
+  imports: [CommonModule, IonicModule, PublicationComponent]
 })
 export class PostDetailsComponent implements OnInit {
-  post: Post = new Post();
+  posts: Post[] = [];
 
   constructor(private route: ActivatedRoute) {
     addIcons({heart, chatbubble, bookmark,shareSocial})
   }
 
   ngOnInit(): void {
+    let post: Post = new Post();
     this.route.paramMap.subscribe(params => {
       let idPost:number = parseInt(params.get('id') || '0', 10);
+      post.id = idPost;
+      console.log('Con id', this.posts);
     });
-    this.post.id = 1;
-    this.post.decription = 'This is a post'
-    this.post.image = 'https://picsum.photos/2000/2000?random=4';
-    this.post.likes = 10;
-    this.post.numComments = 1;
-    this.post.profile.name = 'John Doe';
-    this.post.profile.avatar = 'https://picsum.photos/1000/1000?random=1';
+    post.decription = 'This is a post'
+    post.image = 'https://picsum.photos/2000/1000?random=4';
+    post.likes = 10;
+    post.numComments = 1;
+    post.profile.name = 'John Doe';
+    post.profile.avatar = 'https://picsum.photos/1000/1000?random=1';
 
-    this.post.comments.push({
+    post.comments.push({
       profile: {
         name: 'John Doe',
         avatar: 'https://picsum.photos/1000/1000?random=2'
@@ -40,15 +43,48 @@ export class PostDetailsComponent implements OnInit {
       comment: 'Nice post'
     });
 
-    this.post.comments.push({
+    post.comments.push({
       profile: {
         name: 'Jane Doe',
         avatar: 'https://picsum.photos/1000/1000?random=3'
       },
       comment: 'Nice post'
     })
+    this.posts.push(post);
 
-    console.log(this.post);
+    let post2 = new Post();
+    post2.id = 2;
+    post2.decription = 'This is a post'
+    post2.image = 'https://picsum.photos/2000/1000?random=4';
+    post2.likes = 10;
+    post2.numComments = 1;
+    post2.profile.name = 'John Doe2';
+    post2.profile.avatar = 'https://picsum.photos/1000/1000?random=1';
+
+    post2.comments.push({
+      profile: {
+        name: 'John Doe2',
+        avatar: 'https://picsum.photos/1000/1000?random=2'
+      },
+      comment: 'Nice post'
+    });
+        post2.comments.push({
+      profile: {
+        name: 'John Doe2',
+        avatar: 'https://picsum.photos/1000/1000?random=2'
+      },
+      comment: 'Nice post'
+    });
+
+    post2.comments.push({
+      profile: {
+        name: 'Jane Doe',
+        avatar: 'https://picsum.photos/1000/1000?random=3'
+      },
+      comment: 'Nice post'
+    })
+    this.posts.push(post2);
+    console.log(this.posts);
   }
 
 
