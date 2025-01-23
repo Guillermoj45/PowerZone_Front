ff4b8ded16f10610a9c213dbfdeaaa63c5584bd4
Guillermo <guillermoremesa@gmail.com>
2025-01-23 13:04:01 +0100

    Arreglos de la pantalla post details

--- src/app/Models/Post.ts
+++ src/app/Models/Post.ts
@@ -4,6 +4,7 @@ export class Post {
   profile: Profile = new Profile();
   id: number = 0;
   decription: string = '';
+  image: string = '';
   likes: number = 0;
   numComments: number = 0;
   comments: {
--- src/app/Screen/post-details/post-details.component.ts
+++ src/app/Screen/post-details/post-details.component.ts
@@ -3,6 +3,8 @@ import {ActivatedRoute} from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import {Post} from "../../Models/Post";
+import {addIcons} from "ionicons";
+import {bookmark, chatbubble, heart, shareSocial} from "ionicons/icons";
 
 @Component({
     selector: 'app-post-details',
@@ -14,12 +16,35 @@ import {Post} from "../../Models/Post";
 export class PostDetailsComponent implements OnInit {
   post: Post = new Post();
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {
+    addIcons({heart, chatbubble, bookmark,shareSocial})
+  }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       let idPost:number = parseInt(params.get('id') || '0', 10);
     });
+    this.post.id = 1;
+    this.post.decription = 'This is a post'
+    this.post.image = 'https://picsum.photos/2000/2000?random=4';
+    this.post.likes = 10;
+    this.post.numComments = 1;
+    this.post.comments.push({
+      profile: {
+        name: 'John Doe',
+        avatar: 'https://picsum.photos/1000/1000?random=2'
+      },
+      comment: 'Nice post'
+    });
+
+    this.post.comments.push({
+      profile: {
+        name: 'Jane Doe',
+        avatar: 'https://picsum.photos/1000/1000?random=3'
+      },
+      comment: 'Nice post'
+    })
+
     console.log(this.post);
   }
 
