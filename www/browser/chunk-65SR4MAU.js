8cc283dbcf50f29d181ed406bc8bb9b75450da2f
Fco Javier Montes Estrabón <146116182+tobi1584@users.noreply.github.com>
2025-02-19 19:50:17 +0100

    Delete www directory

--- www/browser/chunk-65SR4MAU.js
+++
@@ -1,7 +0,0 @@
-// node_modules/@popperjs/core/lib/enums.js
-var start = "start";
-
-export {
-  start
-};
-//# sourceMappingURL=chunk-65SR4MAU.js.map
--- www/browser/focus-visible-4BPG5F5S.js
+++
@@ -1,8 +0,0 @@
-import {
-  startFocusVisible
-} from "./chunk-KQEJHESJ.js";
-import "./chunk-ZVATTXSA.js";
-export {
-  startFocusVisible
-};
-//# sourceMappingURL=focus-visible-4BPG5F5S.js.map
